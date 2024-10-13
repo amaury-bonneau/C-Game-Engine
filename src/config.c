b9674b394de65108a30747c7ec753b437836f2f2
amaury.bonneau <amaury.bonneau@gmail.com>
2024-10-13 16:34:20 +0200

    Fix path for JSON config file

--- src/config.c
+++ src/config.c
@@ -13,4 +13,4 @@ const int CHARACTER_STARTING_X = SCREEN_WIDTH/2;
 const int CHARACTER_STARTING_Y = SCREEN_HEIGHT/2;
 
 // Configuration JSON file path
-const char* CONFIG_PATH = "C:/Users/Utilisateur/Dropbox/C_projects/config.json";
+const char* CONFIG_PATH = "./config.json";
